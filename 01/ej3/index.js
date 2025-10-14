035c50f6e88580d5b5142515ecf4ceab30cad754
Nicolas Acero <nicolas.acero2023@gmail.com>
2025-10-14 21:52:23 +0200

    updates

---
+++ 01/ej3/index.js
@@ -0,0 +1,8 @@
+let prompt = require('prompt-sync')();
+
+let numero = parseInt(prompt("Ingrese un numero: "));
+let numero2= parseInt(prompt("Ingrese otro numero: "));
+
+let resultado = numero + numero2;
+
+console.log("El resultado de la suma es: "+ resultado);
