1d465e57e1bd56989d57179409a2dcc3d0ce464d
Nickscodes25 <nicolas.acero2023@gmail.com>
2025-12-08 10:10:26 -0500

    updates

--- Asincrono3b/backend/index.js
+++ Asincrono3b/backend/index.js
@@ -16,28 +16,6 @@ const auth = require('./middlewares/auth');
 
 // Inicialización de Express
 const app = express();
-
-// const allowedOrigins = [
-//   'http://localhost:4173', // frontend local (vite)
-//   'https://asincrono3b-frontend.vercel.app' 
-// ];
-
-//para solucionar problema que CORS no admite
-// app.use(cors({
-//   origin: (origin, callback) => {
-//     if (!origin || allowedOrigins.includes(origin)) {
-//       callback(null, true);
-//     } else {
-//       console.log(' Bloqueado por CORS:', origin);
-//       callback(new Error('No autorizado por CORS'));
-//     }
-//   },
-//   methods: ['GET', 'POST', 'PUT', 'DELETE'],
-//   allowedHeaders: ['Content-Type'],
-// }));
-
-
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(logger);
