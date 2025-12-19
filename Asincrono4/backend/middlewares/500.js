f32d9a9e45373ca8c10038928d8eb72d53a0dce5
Nickscodes25 <nicolas.acero2023@gmail.com>
2025-12-19 14:20:32 -0500

    updates

--- Asincrono4/backend/middlewares/500.js
+++
@@ -1,8 +0,0 @@
-const internalServerError = (err, req, res, next) => {  console.error(err); // Log para debug
-  res.status(500).json({
-    error: "Internal Server Error",
-    message: "Ha ocurrido un error inesperado en el servidor"
-  });
-};
-
-module.exports = internalServerError;
--- Asincrono4/backend/middlewares/logs.js
+++
@@ -1,6 +0,0 @@
-const logger = (req, res, next) => {
-    console.log(`Se ha recibido una pericion de la ip ${req.ip} con el metodo ${req.method} y la url ${req.url}`);
-    next();
-};
-
-module.exports = logger;
